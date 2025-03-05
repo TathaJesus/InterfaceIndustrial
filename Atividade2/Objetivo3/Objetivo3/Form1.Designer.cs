a02e84482c7bb313074f2124a79e57065e127007
ObeliscoAtormentador1 <tamosdejesusjoaovitor@gmail.com>
2025-03-05 19:27:19 -0300

    Atividade 2 completa

--- Atividade2/Objetivo3/Objetivo3/Form1.Designer.cs
+++ Atividade2/Objetivo3/Objetivo3/Form1.Designer.cs
@@ -28,13 +28,96 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.Lime;
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.25F);
+            this.button1.Location = new System.Drawing.Point(60, 333);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(172, 71);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "LIGA";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.Color.Red;
+            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.25F);
+            this.button2.Location = new System.Drawing.Point(312, 333);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(172, 71);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "DESLIGA";
+            this.button2.UseVisualStyleBackColor = false;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.BackColor = System.Drawing.Color.Maroon;
+            this.button3.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.25F);
+            this.button3.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.button3.Location = new System.Drawing.Point(570, 333);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(172, 71);
+            this.button3.TabIndex = 2;
+            this.button3.Text = "SAIR";
+            this.button3.UseVisualStyleBackColor = false;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(230, 9);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(309, 255);
+            this.pictureBox1.TabIndex = 3;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(60, 244);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(123, 20);
+            this.textBox1.TabIndex = 4;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.HighlightText;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
             this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.TextBox textBox1;
     }
 }
 
--- Atividade2/Objetivo3/Objetivo3/Form1.cs
+++ Atividade2/Objetivo3/Objetivo3/Form1.cs
@@ -12,9 +12,56 @@ namespace Objetivo3
 {
     public partial class Form1 : Form
     {
+        private bool ligado = false;
         public Form1()
         {
             InitializeComponent();
+            AtualizarInterface();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            ligado = true;
+            AtualizarInterface();
+            textBox1.Text = "Sistema ligado";
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            ligado = false;
+            AtualizarInterface();
+            textBox1.Text = "Sistema desligado";
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            var confirm = MessageBox.Show("Deseja realmente sair?", "Confirmação", MessageBoxButtons.YesNo, MessageBoxIcon.Question);
+            if (confirm == DialogResult.Yes)
+            {
+                Application.Exit();
+            }
+        }
+        private void AtualizarInterface()
+        {
+            if (ligado)
+            {
+                pictureBox1.Image = Image.FromFile(@"C:/imagens/verde.bmp"); // Bitmap da imagem verde
+            }
+            else
+            {
+                pictureBox1.Image = Image.FromFile(@"C:/imagens/vermelho.bmp"); // Bitmap da imagem vermelha
+            }
+        }
+
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+
         }
     }
 }
