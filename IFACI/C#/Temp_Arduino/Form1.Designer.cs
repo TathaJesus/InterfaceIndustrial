abd43980a6ff70db6a80efeb1ef72f23181f44fd
ObeliscoAtormentador1 <tamosdejesusjoaovitor@gmail.com>
2025-04-02 20:39:02 -0300

    Atividade02/04

---
+++ IFACI/C#/Temp_Arduino/Form1.Designer.cs
@@ -0,0 +1,124 @@
+﻿namespace Temp_Arduino
+{
+    partial class Termômetro
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Termômetro));
+            this.btIniciar = new System.Windows.Forms.Button();
+            this.btFinalizar = new System.Windows.Forms.Button();
+            this.lbPorta = new System.Windows.Forms.Label();
+            this.txPorta = new System.Windows.Forms.TextBox();
+            this.thermControl1 = new ThermometerControl.ThermControl();
+            this.serialPort1 = new System.IO.Ports.SerialPort(this.components);
+            this.SuspendLayout();
+            // 
+            // btIniciar
+            // 
+            this.btIniciar.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Bold);
+            this.btIniciar.Location = new System.Drawing.Point(57, 532);
+            this.btIniciar.Name = "btIniciar";
+            this.btIniciar.Size = new System.Drawing.Size(97, 52);
+            this.btIniciar.TabIndex = 1;
+            this.btIniciar.Text = "Iniciar";
+            this.btIniciar.UseVisualStyleBackColor = true;
+            this.btIniciar.Click += new System.EventHandler(this.btIniciar_Click);
+            // 
+            // btFinalizar
+            // 
+            this.btFinalizar.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Bold);
+            this.btFinalizar.Location = new System.Drawing.Point(210, 532);
+            this.btFinalizar.Name = "btFinalizar";
+            this.btFinalizar.Size = new System.Drawing.Size(97, 52);
+            this.btFinalizar.TabIndex = 2;
+            this.btFinalizar.Text = "Finalizar";
+            this.btFinalizar.UseVisualStyleBackColor = true;
+            this.btFinalizar.Click += new System.EventHandler(this.btFinalizar_Click);
+            // 
+            // lbPorta
+            // 
+            this.lbPorta.AutoSize = true;
+            this.lbPorta.Font = new System.Drawing.Font("Microsoft Sans Serif", 12.25F, System.Drawing.FontStyle.Bold);
+            this.lbPorta.Location = new System.Drawing.Point(53, 466);
+            this.lbPorta.Name = "lbPorta";
+            this.lbPorta.Size = new System.Drawing.Size(102, 20);
+            this.lbPorta.TabIndex = 2;
+            this.lbPorta.Text = "Porta COM";
+            // 
+            // txPorta
+            // 
+            this.txPorta.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F);
+            this.txPorta.Location = new System.Drawing.Point(207, 466);
+            this.txPorta.Name = "txPorta";
+            this.txPorta.Size = new System.Drawing.Size(100, 24);
+            this.txPorta.TabIndex = 3;
+            this.txPorta.Text = "Porta";
+            // 
+            // thermControl1
+            // 
+            this.thermControl1.Location = new System.Drawing.Point(92, 58);
+            this.thermControl1.MinimumSize = new System.Drawing.Size(180, 315);
+            this.thermControl1.Name = "thermControl1";
+            this.thermControl1.Size = new System.Drawing.Size(180, 315);
+            this.thermControl1.TabIndex = 4;
+            this.thermControl1.Value = 0;
+            // 
+            // serialPort1
+            // 
+            this.serialPort1.DataReceived += new System.IO.Ports.SerialDataReceivedEventHandler(this.serialPort1_DataReceived);
+            // 
+            // Termômetro
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.Window;
+            this.ClientSize = new System.Drawing.Size(367, 642);
+            this.Controls.Add(this.thermControl1);
+            this.Controls.Add(this.txPorta);
+            this.Controls.Add(this.lbPorta);
+            this.Controls.Add(this.btFinalizar);
+            this.Controls.Add(this.btIniciar);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Termômetro";
+            this.Text = "Termômetro";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btIniciar;
+        private System.Windows.Forms.Button btFinalizar;
+        private System.Windows.Forms.Label lbPorta;
+        private System.Windows.Forms.TextBox txPorta;
+        private ThermometerControl.ThermControl thermControl1;
+        private System.IO.Ports.SerialPort serialPort1;
+    }
+}
+
---
+++ IFACI/C#/Temp_Arduino/Form1.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO.Ports;
+using System.Drawing.Text;
+
+namespace Temp_Arduino
+{
+    public partial class Termômetro : Form
+    {
+        public Termômetro()
+        {
+            InitializeComponent();//Inicializa componentes do formulário
+        }
+        private void lerTemperatura(object sender, EventArgs e)
+        {
+            byte[] dados = new byte[2];//Buffer para dados (não utilizados)
+            string valor; //Váriavel para armazenamento de leitura
+            valor = serialPort1.ReadExisting();//Lê todos os dados que serão disponibilizados pela porta serial
+            if (valor != "")//Se recebeu dados
+            {
+                thermControl1.UpdateControl(Convert.ToInt32(valor));//Atualiza controle de temperatura
+
+                //Limpa buffer da porta serial
+                serialPort1.DiscardInBuffer();
+                serialPort1.DiscardOutBuffer();
+            }
+        
+        }
+        private void serialPort1_DataReceived(object sender, SerialDataReceivedEventArgs e)
+        {
+            this.Invoke(new EventHandler(lerTemperatura));//Chama ler temperatura na thread da UI
+        }
+        private void btIniciar_Click(object sender, EventArgs e)
+        {
+            if (txPorta.Text != "")//Verifica se foi informado uma porta
+                serialPort1.PortName = txPorta.Text;//Configura a porta serial que foi informada
+            if (!serialPort1.IsOpen)
+                serialPort1.Open();//Abre a conexão serial
+        }
+        private void btFinalizar_Click(object sender, EventArgs e)
+        {
+            serialPort1.Close();//Fecha a conexão serial
+
+            Application.Exit();//Encerra a aplicação
+        }
+    }
+}
